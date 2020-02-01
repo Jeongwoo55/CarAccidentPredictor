e57c0f3ece27592ae574342d98b647f8ec68ed5f
Nathaniel Brown <nateabrown@icloud.com>
2020-02-01 16:13:11 -0600

    Use lower-level compatibility TF

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import pandas as pd
 
 # First we load the entire CSV file into an m x 3
@@ -16,4 +17,59 @@ y_data = D[0:1000, 3].transpose()
 # And make a convenient variable to remember the number of input columns
 n = 2
 
-print(y_data)
+# Define data placeholders
+x = tf.placeholder(tf.float32, shape=(n,None))
+y = tf.placeholder(tf.float32, shape=(1,None))
+
+#Define trainable variables
+A = tf.get_variable("A", shape=(1,n))
+b = tf.get_variable("b", shape=())
+
+#define model output
+y_predicted = tf.matmul(A,x) + b
+
+#define the loss function
+L = tf.reduce_sum((y_predicted - y) ** 2)
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(L)
+
+session = tf.Session()
+session.run(tf.global_variables_initializer())
+
+for t in range(1000):
+    _, current_loss, current_A, current_b = session.run([optimizer, L, A, b], feed_dict = {
+        x: X_data,
+        y: y_data
+    })
+
+print("t = %g, loss = %g, A = %s, b = %g" % (t, current_loss, str(current_A), current_b))
+
+
+# import pandas as pd
+# import torch
+# import torch.optim as optim
+#
+# D = torch.tensor(pd.read_csv("/Users/nathaniel/Downloads/US_Accidents_Dec19.csv", header=None).values, dtype=torch.float)
+# x_dataset = D[0:1000, [29,30]].t()
+# y_dataset = D[0:1000, 3].t()
+# n = 2
+#
+# A = torch.randn((1, n), requires_grad = True)
+# b = torch.randn(1, requires_grad = True)
+#
+# def model(x_input):
+#     return A.mm(x_input) + b
+#
+# def loss(y_predicted, y_target):
+#     return((y_predicted-y_target)**2).sum()
+#
+# optimizer = optim.Adam([A,b], lr=0.1)
+#
+# for t in range(1000):
+#     optimizer.zero_grad()
+#     y_predicted = model(x_dataset)
+#     current_loss = loss(y_predicted, y_dataset)
+#     current_loss.backward()
+#     optimizer.step()
+#     print(f"t = {t}, loss = {current_loss}, A = {A.detach().numpy()}, b = {b.item()}")
+#
